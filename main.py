34c7162f7bdff6d7b9934e8f82dbfb43b2f80012
victor <victordaniel2002@qq.com>
2026-05-30 14:18:25 +0800

    Added circleshape, and modified the main

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -16,6 +18,7 @@ def main():
 
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
